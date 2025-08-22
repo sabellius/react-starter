0d81b46bcc2e065de17c2f2935e3703fb3cc39f0
sabellius128 <50418272+sabellius@users.noreply.github.com>
2025-08-22 10:42:37 +0300

    feat: ex1 - display an AnimalList component in the home page

---
+++ cmps/AnimalList.jsx
@@ -0,0 +1,29 @@
+const AnimalList = ({ animalInfos }) => {
+  return (
+    <div className="table-container">
+      <h1>Rare Animals</h1>
+      <table className="animal-list">
+        <tbody>
+          {animalInfos.map(({ type, count }) => {
+            return (
+              <tr key={type}>
+                <td>{type}</td>
+                <td>{count}</td>
+                <td>
+                  <a
+                    href={`https://www.google.com/search?q=${type}`}
+                    target="_blank"
+                  >
+                    Search
+                  </a>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default AnimalList;
