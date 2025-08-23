62f763720f1e054e5c84dd4421d4a4cbd59c46e4
Saveliy Shiryaev <50418272+sabellius@users.noreply.github.com>
2025-08-23 08:47:01 +0300

    feat: play a sound when counter reaches 0

--- cmps/CountDown.jsx
+++ cmps/CountDown.jsx
@@ -5,6 +5,7 @@ const CountDown = ({ startFrom, onDone, toTime = null }) => {
     toTime ? (toTime - Date.now()) / 1000 : startFrom
   );
   const counterRef = useRef(null);
+  const audioRef = useRef(null);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -12,6 +13,7 @@ const CountDown = ({ startFrom, onDone, toTime = null }) => {
         if (currentCount <= 0) {
           clearInterval(interval);
           onDone(counterRef);
+          audioRef.current.play();
           return 0;
         }
         return currentCount - 1;
@@ -29,6 +31,7 @@ const CountDown = ({ startFrom, onDone, toTime = null }) => {
           className={`count ${count <= 6 ? 'color-red' : ''}`}>
           {count}
         </div>
+        <audio ref={audioRef} src="assets/audio/ship-horn.mp3"></audio>
       </div>
     </div>
   );
