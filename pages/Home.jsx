2dac2cb830c69eff119a46891a3ff29a201412fe
Saveliy Shiryaev <50418272+sabellius@users.noreply.github.com>
2025-08-24 16:18:30 +0300

    fix: display all components at the same time

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -14,15 +14,14 @@ const animalInfos = [
 export function Home() {
   return (
     <section className="home">
-      {/* <h2>Home Sweet Home</h2> */}
-      {/* <AnimalList animalInfos={animalInfos} /> */}
-      {/* <SeasonClock /> */}
-      {/* <CountDown
+      <AnimalList animalInfos={animalInfos} />
+      <SeasonClock />
+      <CountDown
         startFrom={10}
         toTime={Date.now() + 5 * 1000}
         onDone={ref => utilService.animateCSS(ref.current)}
-      /> */}
-      {/* <WatcherApp /> */}
+      />
+      <WatcherApp />
       <MouseMonitor />
     </section>
   );
